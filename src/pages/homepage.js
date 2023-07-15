222c8c6449f0460aed3c5fc4529d0cbee3b07a94
Ákos Lukács <81911776+lukiakos@users.noreply.github.com>
2023-07-15 20:56:36 +0200

    Finished the homepage
    The JS now fully renders the homepage.
    I turned the images into PNGs, because they are easier to work with than SVGs for now.

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,4 +1,7 @@
-import wifisvg from '../resources/wifi.svg'
+import { loadNextPage } from '../components/loadPage';
+import wifiImg from '../resources/wifi.png';
+import chargeImg from '../resources/battery.png';
+import bookImg from '../resources/bookcorner.png';
 
 const renderHomepage = () => {
     //set up main grid
@@ -21,20 +24,161 @@ const renderHomepage = () => {
     servicesGrid.id='servicesgrid';
     leftSideFloatie.appendChild(servicesGrid);
 
-    //add servicesgrid content
+    //add servicesgrid element 1 (Free WiFi)
     const servicesgridelement1 = document.createElement('div');
     servicesgridelement1.classList.add('serviceGridElement');
     servicesGrid.appendChild(servicesgridelement1);
     const wifiImage = document.createElement('img');
-    wifiImage.src = "url('../resources/wifi.svg')";
-    wifiImage.width='69px';
-    wifiImage.height='69px';
-    wifiImage.alt='wifi';
+    wifiImage.src = wifiImg;
+    wifiImage.width = '69';
+    wifiImage.height = '69';
+    wifiImage.alt = 'wifi';
     servicesgridelement1.appendChild(wifiImage);
     const wifiText = document.createElement('p');
     wifiText.textContent='Free WiFi';
     servicesgridelement1.appendChild(wifiText);
 
+    //add servicesgrid element 2 (ChargeUp)
+    const servicesgridelement2 = document.createElement('div');
+    servicesgridelement2.classList.add('serviceGridElement');
+    servicesGrid.appendChild(servicesgridelement2);
+    const chargeImage = document.createElement('img');
+    chargeImage.src = chargeImg;
+    chargeImage.width = '69';
+    chargeImage.height= '69';
+    chargeImage.alt = 'charge';
+    servicesgridelement2.appendChild(chargeImage);
+    const chargeText = document.createElement('p');
+    chargeText.textContent = 'ChargeUp';
+    servicesgridelement2.appendChild(chargeText);
+
+    //add servicesgrid element 3 (BookCorner)
+    const servicesgridelement3 = document.createElement('div');
+    servicesgridelement3.classList.add('serviceGridElement');
+    servicesGrid.appendChild(servicesgridelement3);
+    const bookImage = document.createElement('img');
+    bookImage.src = bookImg;
+    bookImage.width = '60';
+    bookImage.height= '69';
+    bookImage.alt = 'books';
+    servicesgridelement3.appendChild(bookImage);
+    const bookText = document.createElement('p');
+    bookText.textContent = 'BookCorner';
+    servicesgridelement3.appendChild(bookText);
+
+    //add left floatie button
+    const leftFloatieButton = document.createElement('button');
+    leftFloatieButton.id = 'leftFloatieButton';
+    leftFloatieButton.classList.add('floatiebutton');
+    leftFloatieButton.textContent = 'About Us';
+    leftFloatieButton.addEventListener('click', () => {
+        loadNextPage('aboutus')
+    });
+    leftSideFloatie.appendChild(leftFloatieButton);
+
+    //set up middle floatie
+    const middleFloatie = document.createElement('div');
+    middleFloatie.id = "middlefloatie";
+    mainGrid.appendChild(middleFloatie);
+    const middleTitle = document.createElement('h1');
+    middleTitle.textContent = 'KoffeeCorner is...';
+    middleFloatie.appendChild(middleTitle);
+
+    //create ul
+    const parentList = document.createElement('ul');
+    middleFloatie.appendChild(parentList);
+
+    //create list items
+    const middleItem1 = document.createElement('li');
+    middleItem1.innerHTML = '... the most <a class="orangetext">cozy</a> spot in town';
+    const middleItem2 = document.createElement('li');
+    middleItem2.innerHTML = '... the best place to <a class="orangetext">get work done';
+    const middleItem3 = document.createElement('li');
+    middleItem3.innerHTML = '... the <a class="orangetext">heart</a> of the city';
+    const middleItem4 = document.createElement('li');
+    middleItem4.innerHTML = '... the home of <a class="orangetext">the best Koffee</a>';
+    const middleItem5 = document.createElement('li');
+    middleItem5.innerHTML = '... all about the <a class="orangetext">community</a>';
+    
+    //append list items to ul
+    parentList.appendChild(middleItem1);
+    parentList.appendChild(middleItem2);
+    parentList.appendChild(middleItem3);
+    parentList.appendChild(middleItem4);
+    parentList.appendChild(middleItem5);
+
+    //add two last h2s to middle floatie
+    const middleBreakTitle = document.createElement('h2');
+    middleBreakTitle.innerHTML = 'Catch a break<br/>or work in harmony';
+    middleFloatie.appendChild(middleBreakTitle);
+    const middleChoiceTitle = document.createElement('h2');
+    middleChoiceTitle.classList.add('orangetext');
+    middleChoiceTitle.textContent = 'The choice is yours, really';
+    middleFloatie.appendChild(middleChoiceTitle);
+
+    //set up right floatie
+    const rightSideFloatie = document.createElement('div');
+    rightSideFloatie.classList.add('sidefloatie');
+    mainGrid.appendChild(rightSideFloatie);
+    const rightSideFloatieTitle = document.createElement('h2');
+    rightSideFloatieTitle.id = 'offersTitle';
+    rightSideFloatieTitle.textContent = 'Best Offers';
+    rightSideFloatie.appendChild(rightSideFloatieTitle);
+
+    //set up offersgrid
+    const offersGrid = document.createElement('div');
+    offersGrid.id = 'offersgrid';
+    rightSideFloatie.appendChild(offersGrid);
+
+    //set up offersgrid elements
+    //element 1
+    const offersGridElement1 = document.createElement('div');
+    offersGridElement1.classList.add('offersGridElement');
+    offersGrid.appendChild(offersGridElement1);
+    const offersGridElement1Title = document.createElement('p');
+    offersGridElement1Title.innerHTML = 'Corner<br>Special';
+    offersGridElement1.appendChild(offersGridElement1Title);
+    const offersGridElement1Price = document.createElement('p');
+    offersGridElement1Price.textContent = '2,00€';
+    offersGridElement1.appendChild(offersGridElement1Price);
+    //element 2
+    const offersGridElement2 = document.createElement('div');
+    offersGridElement2.classList.add('offersGridElement');
+    offersGrid.appendChild(offersGridElement2);
+    const offersGridElement2Title = document.createElement('p');
+    offersGridElement2Title.innerHTML = 'Kold<br>Brew';
+    offersGridElement2.appendChild(offersGridElement2Title);
+    const offersGridElement2Price = document.createElement('p');
+    offersGridElement2Price.textContent = '3,50€';
+    offersGridElement2.appendChild(offersGridElement2Price);
+    //element 3
+    const offersGridElement3 = document.createElement('div');
+    offersGridElement3.classList.add('offersGridElement');
+    offersGrid.appendChild(offersGridElement3);
+    const offersGridElement3Title = document.createElement('p');
+    offersGridElement3Title.innerHTML = 'Klassic<br>Koffee';
+    offersGridElement3.appendChild(offersGridElement3Title);
+    const offersGridElement3Price = document.createElement('p');
+    offersGridElement3Price.textContent = '1,50€';
+    offersGridElement3.appendChild(offersGridElement3Price);
+    
+    //rightsidefloatiebutton
+    const rightSideFloatieButton = document.createElement('button');
+    rightSideFloatieButton.classList.add('floatiebutton');
+    rightSideFloatieButton.id = 'rightFloatieButton';
+    rightSideFloatieButton.textContent = 'Menu';
+    rightSideFloatie.appendChild(rightSideFloatieButton);
+
+    //add clickability
+    const offersGridElements = document.querySelectorAll('.offersGridElement');
+    offersGridElements.forEach((e) => {
+        e.addEventListener('click', () => {
+            loadNextPage('menu');
+        });
+    });
+    rightSideFloatieButton.addEventListener('click', () => {
+        loadNextPage('menu');
+    })
 
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
         test: /\.svg$/,
         loader: 'svg-inline-loader'
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
 };
