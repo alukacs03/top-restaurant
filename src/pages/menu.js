b2aea7ac626babc73ae52a6a2ddce8e0169fb748
Ákos Lukács <81911776+lukiakos@users.noreply.github.com>
2023-07-21 21:39:35 +0200

    set up structure for the menu, TBD: add dummy content

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,41 +3,77 @@ const renderMenu = () => {
     const content = document.querySelector('#content');
     const menuGrid = document.createElement('div');
     menuGrid.id = 'menugrid';
+    const menuTitleDiv = document.createElement('div');
+    menuTitleDiv.id = 'menuTitleDiv';
     content.appendChild(menuGrid);
+    menuGrid.appendChild(menuTitleDiv);
     const menuTitle = document.createElement('h1');
     menuTitle.id = "menuTitle"
-    menuTitle.textContent = 'Take a gander and follow your heart..'
-    menuGrid.appendChild(menuTitle);
+    menuTitle.textContent = 'Take a gander and follow your heart...'
+    menuTitleDiv.appendChild(menuTitle);
 
     //set up first item
     const itemOne = document.createElement('div');
     itemOne.classList.add('menuElement');
     menuGrid.appendChild(itemOne);
+    const itemOneBg = document.createElement('div');
+    itemOneBg.classList.add('menuItemBackground');
+    itemOne.appendChild(itemOneBg);
+    const itemOneName = document.createElement('h2');
+    itemOneName.innerHTML = 'Corner <br> Special';
+    itemOneName.classList.add('menuItemName');
+    itemOneBg.appendChild(itemOneName);
+    const dividingLineOne = document.createElement('div')
+    dividingLineOne.classList.add('menuDividingLine')
+    itemOneBg.appendChild(dividingLineOne)
+    const itemOneDescription = document.createElement('p');
+    itemOneDescription.textContent = 'Consectetur et in dolore excepteur voluptate ea labore est pariatur mollit ea officia. Veniam cupidatat pariatur aute est. Est aute do nulla ullamco irure ea deserunt. Est sit exercitation dolore exercitation officia dolore irure ad proident laborum Lorem eiusmod dolore commodo.'
+    itemOneDescription.classList.add('menuItemDescription');
+    itemOneBg.appendChild(itemOneDescription);
+    const itemOnePrice = document.createElement('p');
+    itemOnePrice.textContent = '2,00€';
+    itemOnePrice.classList.add('menuItemPrice');
+    itemOneBg.appendChild(itemOnePrice)
 
     //set up second item
     const itemTwo = document.createElement('div');
     itemTwo.classList.add('menuElement');
     menuGrid.appendChild(itemTwo);
+    const itemTwoBg = document.createElement('div');
+    itemTwoBg.classList.add('menuItemBackground');
+    itemTwo.appendChild(itemTwoBg);
 
     //third item
     const itemThree = document.createElement('div');
     itemThree.classList.add('menuElement');
     menuGrid.appendChild(itemThree);
+    const itemThreeBg = document.createElement('div');
+    itemThreeBg.classList.add('menuItemBackground');
+    itemThree.appendChild(itemThreeBg);
 
     //fourth item
     const itemFour = document.createElement('div');
     itemFour.classList.add('menuElement');
     menuGrid.appendChild(itemFour);
+    const itemFourBg = document.createElement('div');
+    itemFourBg.classList.add('menuItemBackground');
+    itemFour.appendChild(itemFourBg);
 
     //fifth item
     const itemFive = document.createElement('div');
     itemFive.classList.add('menuElement');
     menuGrid.appendChild(itemFive);
+    const itemFiveBg = document.createElement('div');
+    itemFiveBg.classList.add('menuItemBackground');
+    itemFive.appendChild(itemFiveBg);
 
     //sixth item
     const itemSix = document.createElement('div');
     itemSix.classList.add('menuElement');
     menuGrid.appendChild(itemSix);
+    const itemSixBg = document.createElement('div');
+    itemSixBg.classList.add('menuItemBackground');
+    itemSix.appendChild(itemSixBg);
 }
 
 export default renderMenu;
