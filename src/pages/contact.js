0629e1e6c504a0ed4e51c9afd23a538a55cfa040
Ákos Lukács <81911776+lukiakos@users.noreply.github.com>
2023-07-22 20:35:35 +0200

    test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,40 @@
+import { name } from "file-loader";
+
 const renderContact = () => {
     //basic grid setup
     const content = document.querySelector('#content');
     const contactGrid = document.createElement('div');
     contactGrid.id = 'contactgrid';
     content.appendChild(contactGrid);
-    const contactTitle = document.createElement('h1');
-    contactTitle.id = "contactTitle"
-    contactTitle.textContent = 'Contact us!'
-    contactGrid.appendChild(contactTitle);
+
+    //render short floatie on the left
+    const shortFloatie = document.createElement('div');
+    shortFloatie.classList.add('sidefloatie');
+    shortFloatie.id = 'contactUsFloatie';
+    contactGrid.appendChild(shortFloatie);
+    const shortStoryTitle = document.createElement('h2');
+    shortStoryTitle.classList.add('sideFloatieTitle');
+    shortStoryTitle.textContent = 'Contact Us';
+    shortFloatie.appendChild(shortStoryTitle);
+
+    const contactUsText = document.createElement('p');
+    contactUsText.textContent = 'Ad id Lorem dolore exercitation enim do magna. Nisi reprehenderit adipisicing irure laborum do ad ad esse est labore ut laboris. Minim aute reprehenderit aliqua labore est culpa incididunt culpa ipsum in. In aute incididunt dolor excepteur consectetur esse anim deserunt laborum dolore esse.';
+    contactUsText.id = 'contactUsText'
+    shortFloatie.appendChild(contactUsText);
+
+    //render the right floatie, on which all the services are explained with more detail
+    const rightFloatie = document.createElement('div');
+    rightFloatie.id = "contactUsRightFloatie";
+
+    contactGrid.appendChild(rightFloatie);
+
+    //fill the right floatie
+    const contactRightFloatieTitle = document.createElement('h2')
+    contactRightFloatieTitle.id = "contactRightFloatieTitle"
+    contactRightFloatieTitle.innerHTML = 'Got any questions? <br> Send us an email!'
+    rightFloatie.appendChild(contactRightFloatieTitle)
+
+
 }
 
 export default renderContact;
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -42,6 +42,21 @@ const renderMenu = () => {
     const itemTwoBg = document.createElement('div');
     itemTwoBg.classList.add('menuItemBackground');
     itemTwo.appendChild(itemTwoBg);
+    const itemTwoName = document.createElement('h2');
+    itemTwoName.innerHTML = 'Kold <br> Brew';
+    itemTwoName.classList.add('menuItemName');
+    itemTwoBg.appendChild(itemTwoName);
+    const dividingLineTwo = document.createElement('div')
+    dividingLineTwo.classList.add('menuDividingLine')
+    itemTwoBg.appendChild(dividingLineTwo)
+    const itemTwoDescription = document.createElement('p');
+    itemTwoDescription.textContent = 'Consectetur et in dolore excepteur voluptate ea labore est pariatur mollit ea officia. Veniam cupidatat pariatur aute est. Est aute do nulla ullamco irure ea deserunt. Est sit exercitation dolore exercitation officia dolore irure ad proident laborum Lorem eiusmod dolore commodo.'
+    itemTwoDescription.classList.add('menuItemDescription');
+    itemTwoBg.appendChild(itemTwoDescription);
+    const itemTwoPrice = document.createElement('p');
+    itemTwoPrice.textContent = '3,50€';
+    itemTwoPrice.classList.add('menuItemPrice');
+    itemTwoBg.appendChild(itemTwoPrice)
 
     //third item
     const itemThree = document.createElement('div');
@@ -50,6 +65,21 @@ const renderMenu = () => {
     const itemThreeBg = document.createElement('div');
     itemThreeBg.classList.add('menuItemBackground');
     itemThree.appendChild(itemThreeBg);
+    const itemThreeName = document.createElement('h2');
+    itemThreeName.innerHTML = 'Klassic <br> Koffee';
+    itemThreeName.classList.add('menuItemName');
+    itemThreeBg.appendChild(itemThreeName);
+    const dividingLineThree = document.createElement('div')
+    dividingLineThree.classList.add('menuDividingLine')
+    itemThreeBg.appendChild(dividingLineThree)
+    const itemThreeDescription = document.createElement('p');
+    itemThreeDescription.textContent = 'Consectetur et in dolore excepteur voluptate ea labore est pariatur mollit ea officia. Veniam cupidatat pariatur aute est. Est aute do nulla ullamco irure ea deserunt. Est sit exercitation dolore exercitation officia dolore irure ad proident laborum Lorem eiusmod dolore commodo.'
+    itemThreeDescription.classList.add('menuItemDescription');
+    itemThreeBg.appendChild(itemThreeDescription);
+    const itemThreePrice = document.createElement('p');
+    itemThreePrice.textContent = '1,50€';
+    itemThreePrice.classList.add('menuItemPrice');
+    itemThreeBg.appendChild(itemThreePrice)
 
     //fourth item
     const itemFour = document.createElement('div');
@@ -58,6 +88,21 @@ const renderMenu = () => {
     const itemFourBg = document.createElement('div');
     itemFourBg.classList.add('menuItemBackground');
     itemFour.appendChild(itemFourBg);
+    const itemFourName = document.createElement('h2');
+    itemFourName.innerHTML = 'Corner <br> Kappuccino';
+    itemFourName.classList.add('menuItemName');
+    itemFourBg.appendChild(itemFourName);
+    const dividingLineFour = document.createElement('div')
+    dividingLineFour.classList.add('menuDividingLine')
+    itemFourBg.appendChild(dividingLineFour)
+    const itemFourDescription = document.createElement('p');
+    itemFourDescription.textContent = 'Consectetur et in dolore excepteur voluptate ea labore est pariatur mollit ea officia. Veniam cupidatat pariatur aute est. Est aute do nulla ullamco irure ea deserunt. Est sit exercitation dolore exercitation officia dolore irure ad proident laborum Lorem eiusmod dolore commodo.'
+    itemFourDescription.classList.add('menuItemDescription');
+    itemFourBg.appendChild(itemFourDescription);
+    const itemFourPrice = document.createElement('p');
+    itemFourPrice.textContent = '1,75€';
+    itemFourPrice.classList.add('menuItemPrice');
+    itemFourBg.appendChild(itemFourPrice)
 
     //fifth item
     const itemFive = document.createElement('div');
@@ -66,6 +111,21 @@ const renderMenu = () => {
     const itemFiveBg = document.createElement('div');
     itemFiveBg.classList.add('menuItemBackground');
     itemFive.appendChild(itemFiveBg);
+    const itemFiveName = document.createElement('h2');
+    itemFiveName.innerHTML = 'Corner <br> Amerikano';
+    itemFiveName.classList.add('menuItemName');
+    itemFiveBg.appendChild(itemFiveName);
+    const dividingLineFive = document.createElement('div')
+    dividingLineFive.classList.add('menuDividingLine')
+    itemFiveBg.appendChild(dividingLineFive)
+    const itemFiveDescription = document.createElement('p');
+    itemFiveDescription.textContent = 'Consectetur et in dolore excepteur voluptate ea labore est pariatur mollit ea officia. Veniam cupidatat pariatur aute est. Est aute do nulla ullamco irure ea deserunt. Est sit exercitation dolore exercitation officia dolore irure ad proident laborum Lorem eiusmod dolore commodo.'
+    itemFiveDescription.classList.add('menuItemDescription');
+    itemFiveBg.appendChild(itemFiveDescription);
+    const itemFivePrice = document.createElement('p');
+    itemFivePrice.textContent = '2,00€';
+    itemFivePrice.classList.add('menuItemPrice');
+    itemFiveBg.appendChild(itemFivePrice)
 
     //sixth item
     const itemSix = document.createElement('div');
@@ -74,6 +134,21 @@ const renderMenu = () => {
     const itemSixBg = document.createElement('div');
     itemSixBg.classList.add('menuItemBackground');
     itemSix.appendChild(itemSixBg);
+    const itemSixName = document.createElement('h2');
+    itemSixName.innerHTML = 'Turkish <br> Koffee';
+    itemSixName.classList.add('menuItemName');
+    itemSixBg.appendChild(itemSixName);
+    const dividingLineSix = document.createElement('div')
+    dividingLineSix.classList.add('menuDividingLine')
+    itemSixBg.appendChild(dividingLineSix)
+    const itemSixDescription = document.createElement('p');
+    itemSixDescription.textContent = 'Consectetur et in dolore excepteur voluptate ea labore est pariatur mollit ea officia. Veniam cupidatat pariatur aute est. Est aute do nulla ullamco irure ea deserunt. Est sit exercitation dolore exercitation officia dolore irure ad proident laborum Lorem eiusmod dolore commodo.'
+    itemSixDescription.classList.add('menuItemDescription');
+    itemSixBg.appendChild(itemSixDescription);
+    const itemSixPrice = document.createElement('p');
+    itemSixPrice.textContent = '2,50€';
+    itemSixPrice.classList.add('menuItemPrice');
+    itemSixBg.appendChild(itemSixPrice)
 }
 
 export default renderMenu;
