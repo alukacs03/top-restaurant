616fd0d6a82de3feb8f06ecba41e742834db0724
Ákos Lukács <81911776+lukiakos@users.noreply.github.com>
2023-07-23 18:00:41 +0200

    added contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -32,6 +32,58 @@ const renderContact = () => {
     contactRightFloatieTitle.innerHTML = 'Got any questions? <br> Send us an email!'
     rightFloatie.appendChild(contactRightFloatieTitle)
 
+    const contactForm = document.createElement('form');
+    contactForm.method = 'post';
+    contactForm.id = 'contactForm';
+    rightFloatie.appendChild(contactForm);
+
+    const nameLabel = document.createElement('label');
+    nameLabel.innerHTML = 'Name';
+    nameLabel.htmlFor = 'name';
+    nameLabel.id = 'nameLabel'
+    const nameInput = document.createElement('input');
+    nameInput.type = 'text';
+    nameInput.maxLength = 25;
+    nameInput.minLength = 3;
+    nameInput.id = 'name';
+    nameInput.required = 'true';
+    nameInput.placeholder = 'Write your name here...'
+    nameInput.classList.add('contactInputField');
+    contactForm.appendChild(nameLabel);
+    contactForm.appendChild(nameInput);
+
+    const emailLabel = document.createElement('label');
+    emailLabel.innerHTML = 'Email';
+    emailLabel.htmlFor = 'email';
+    emailLabel.id = 'emailLabel';
+    const emailInput = document.createElement('input');
+    emailInput.type = 'email';
+    emailInput.maxLength = 50;
+    emailInput.minLength = 3;
+    emailInput.id = 'email';
+    emailInput.required = 'true';
+    emailInput.placeholder = 'name@mail.com'
+    emailInput.classList.add('contactInputField');
+    contactForm.appendChild(emailLabel);
+    contactForm.appendChild(emailInput);
+
+    const messageLabel = document.createElement('label');
+    messageLabel.innerHTML = 'Message';
+    messageLabel.htmlFor = 'message';
+    messageLabel.id = 'messageLabel';
+    const messageInput = document.createElement('textarea');
+    messageInput.id = 'message';
+    messageInput.classList.add('contactInputField');
+    messageInput.required = 'true';
+    messageInput.placeholder = 'Write your message here...'
+    contactForm.appendChild(messageLabel);
+    contactForm.appendChild(messageInput);
+
+    const sendButton = document.createElement('input');
+    sendButton.id = 'contactSendButton';
+    sendButton.type = 'submit';
+    sendButton.value = 'Send message';
+    contactForm.appendChild(sendButton);
 
 }
 
